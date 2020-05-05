deb27919eb705a10f6767cbb61023cd19a6599ec
Daniel Barlow <dan@telent.net>
2020-05-06 00:02:19 +0100

    add a systemd user service for configure-firefox
    I *think* this is working.  After running
    
        nix-env -r -iA nixos.desktop
    
    you now need to do
    
        systemctl --user enable configure-firefox
    
    (and I guess, on each subsequent deploy that changes the settings,
    
        systemctl --user reload configure-firefox
    )

--- pkgs/configure-firefox/default.nix
+++ pkgs/configure-firefox/default.nix
@@ -1,12 +1,14 @@
-{ stdenv, pkgs }:
+{ stdenv, pkgs, python37, wrapGAppsHook }:
 stdenv.mkDerivation {
-  buildInputs = [ pkgs.python37 ];
+  buildInputs = [ python37 ];
+  nativeBuildInputs = [ wrapGAppsHook ];
   pname = "configure-firefox";
   version = "1";
   src = ./.;
   unpackPhase = ":";
   installPhase = ''
-  echo install -m755 -D ${./configure-firefox.py} $out/bin/configure-firefox
+  mkdir -p $out/share/systemd/user
+  substitute ${./configure-firefox.service} $out/share/systemd/user/configure-firefox.service --replace @CONFIGURE-FIREFOX@ $out/bin/configure-firefox
   install -m755 -D ${./configure-firefox.py} $out/bin/configure-firefox
 '';
 
